f9c880f8d42c39d958032567bf825d0828699c7d
Rowan Hussein <rowannhusseinn@gmail.com>
2025-09-01 00:58:37 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -26,17 +26,4 @@ create table Order_Items(
 	quantity int,
 	price decimal 
 )
-drop table Products
-drop table Customers
-drop table Orders
-drop table Order_Items
 
-select * from Orders
-select * from Order_Items
-select * from Products
-
-
-select oi.product_id, p.name, oi.quantity, oi.price
-                      from Order_Items oi
-                      join Products p ON oi.product_id = p.product_id
-                      where oi.order_id = 1
